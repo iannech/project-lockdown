d5b31e6cfb3a2ca0a89d201ce37f43da208d0e06
Ian Nechenje <inechenje@safaricom.co.ke>
2020-03-27 21:37:50 +0300

    OTP-Auth Service: Initial Commit

---
+++ project-lockdown-otp/src/main/java/com/org/projectlockdownotp/ProjectLockdownOtpApplication.java
@@ -0,0 +1,13 @@
+package com.org.projectlockdownotp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLockdownOtpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLockdownOtpApplication.class, args);
+    }
+
+}
---
+++ project-lockdown-otp/src/test/java/com/org/projectlockdownotp/ProjectLockdownOtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.projectlockdownotp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLockdownOtpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

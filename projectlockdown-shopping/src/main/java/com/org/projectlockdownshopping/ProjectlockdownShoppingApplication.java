627c2565af3b9a298b2fe3922d96a731f1c2c4ba
Ian Nechenje <inechenje@safaricom.co.ke>
2020-03-31 21:04:18 +0300

    scaffolding in progress

---
+++ projectlockdown-shopping/src/main/java/com/org/projectlockdownshopping/ProjectlockdownShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.org.projectlockdownshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectlockdownShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectlockdownShoppingApplication.class, args);
+    }
+
+}
---
+++ projectlockdown-shopping/src/test/java/com/org/projectlockdownshopping/ProjectlockdownShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.projectlockdownshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectlockdownShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
